f2fc5f60d8c1978b3bbd0169ea8df1cc4486ef1a
Sherry <sherry@lulainno.com>
2021-09-02 23:20:12 +0800

    新增贊助版塊

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
-//register service worker
+window.onload = () => {
+  new ClipboardJS('.copy');
+
+  popDonate = (elm) => {
+    /** @type HTMLElement */
+    let platform = elm.getAttribute("platform");
+    let copyContent = elm.getAttribute("data-clipboard-text");
+    Swal.fire(
+      `謝謝你的${platform}贊助`,
+      `幫您把 ${copyContent} 複製起來了`,
+      'success'
+    );
+  }
+};
+
+// register service worker
 navigator.serviceWorker.register('service-worker.js', { scope: "." });
--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["images/instagram.png","8ffc1c8179ac2bbe0b4dcd43c1847446"],["images/mail.png","8936fc635ff29fb0fe01dcc7afea047a"],["images/mewe.png","d48b0c1754e24f23bdf76c2151a7b3ba"],["images/telegram.png","f37fbb46904c6d5ed7766300bb87a8b9"],["images/twitter.png","16905c87375d906d060d9e553c1b7f48"],["index.html","01d1d62d69ab5dc47e7a26615140e88c"],["style.css","7cea072e52908396db577ae8eba05501"]];
+var precacheConfig = [["images/instagram.png","8ffc1c8179ac2bbe0b4dcd43c1847446"],["images/mail.png","8936fc635ff29fb0fe01dcc7afea047a"],["images/mewe.png","d48b0c1754e24f23bdf76c2151a7b3ba"],["images/telegram.png","f37fbb46904c6d5ed7766300bb87a8b9"],["images/twitter.png","16905c87375d906d060d9e553c1b7f48"],["index.html","141b942538a70e4f83cc376840fe4976"],["style.css","93b56a341de67940beca0e6a22c2d646"]];
 var cacheName = 'sw-precache-v3-sw-precache-' + (self.registration ? self.registration.scope : '');
 
 
