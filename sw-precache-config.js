1ce1004b0bf04a965a3024708d2bf8023544535f
Sherry <sherry@lulainno.com>
2022-07-18 23:25:58 +0800

    更新sw設定

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -9,7 +9,7 @@ module.exports = {
     ],
     runtimeCaching: [{
         urlPattern: /.*/,
-        handler: 'networkFirst'
+        handler: 'fastest'
     }],
     swFile: 'service-worker.js'
 };
